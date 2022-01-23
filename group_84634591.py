c0c325862c6736f5a6dbfc4151eb9579ff3023b0
Sam Jarkas <55114284+AbdulwahabJarkas@users.noreply.github.com>
2022-01-23 17:05:51 -0500

    Error handling for invalid request and define arguments

--- group_84634591.py
+++ group_84634591.py
@@ -18,18 +18,23 @@ def run(data, bot_info, send):
                  bot_info[0])
 
         elif cmd == 'define' or cmd == 'request':
-            ws = bot_info[2].get_worksheet(0)
-            term = data['text'].split(' ', 1)[1]
 
-            #Check Definitions, then aliases
+            #Initalize Spreadsheet
+            ws = bot_info[2].get_worksheet(0)
+
+            #Get Argument Error Handling
+            term = data['text'].split(' ', 1)
+            if len(term) == 1 or not term[1].strip():
+                send("Invalid command formatting, type !help for more instructions", bot_info[0])
+            term = term[1]
+
+            #Check Definitions, then aliases. This section is really bad.
             loc = ws.find(re.compile(r'^\s*{}\s*$'.format(term), re.I))
             if not loc:
                 loc = ws.find(re.compile(r'\b{}\b'.format(term), re.I))
-
-
-            #TODO Add error handling for incorrect argument
-            if not term.strip():
-                return
+                #remove loc if its an invalid result from "Term" column
+                if loc and cmd == 'request':
+                    loc = None if loc.col == 1 else loc
 
             if loc:
                 definition = ws.cell(loc.row, 2)
@@ -37,7 +42,6 @@ def run(data, bot_info, send):
                 if definition.value:
                     send("@{}, the definition of {} is: \n\n{}".format(data['name'], loc.value, definition.value)
                          , bot_info[0])
-
                 else:
                     send("@{}, term '{}' has been requested but not yet defined".
                          format(data['name'], term), bot_info[0])
