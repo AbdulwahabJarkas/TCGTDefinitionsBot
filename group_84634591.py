10e1c971f5d9bb9c871357dbca2898dfd8f663bf
Sam Jarkas <55114284+AbdulwahabJarkas@users.noreply.github.com>
2022-01-31 16:25:33 -0500

    Pushing Refactored Command System To Live Bot

--- group_84634591.py
+++ group_84634591.py
@@ -50,10 +50,12 @@ def define(data, ws):
     #Check For Definition
     if loc:
         definition = ws.cell(loc.row, 2)
+        if loc.col != 1:
+            loc = ws.cell(loc.row, 1)
 
         if definition.value:
-            return "@{}, the definition of {} is: \n\n{}"\
-                       .format(data['name'], loc.value, definition.value), attachment
+            return "@{a}, the definition of {b} is: \n\n{c}"\
+                       .format(a=data['name'], b=loc.value, c=definition.value), attachment
         else:
             return "@{}, term '{}' has been requested but not yet defined"\
                        .format(data['name'], term), attachment
@@ -81,6 +83,8 @@ def request(data, ws):
 
     #Check For Definition
     if loc:
+        if loc.col != 1:
+            loc = ws.cell(loc.row, 1)
         definition = ws.cell(loc.row, 2)
 
         if definition.value:
@@ -100,46 +104,3 @@ def request(data, ws):
                "Thank you for your contribution".format(data['name'], term), attachment
 
 
-        # elif cmd == 'define' or cmd == 'request':
-        #
-        #     #Initalize Spreadsheet
-        #     ws = bot_info[2].get_worksheet(0)
-        #
-        #     #Get Argument Error Handling
-        #     term = data['text'].split(' ', 1)
-        #     if len(term) == 1 or not term[1].strip():
-        #         send("Invalid command formatting, type !help for more instructions", bot_info[0])
-        #         return
-        #     term = term[1]
-        #
-        #     #Check Definitions, then aliases. This section is really bad.
-        #     loc = ws.find(re.compile(r'^\s*{}\s*$'.format(term), re.I))
-        #     if not loc:
-        #         loc = ws.find(re.compile(r'\b{}\b'.format(term), re.I))
-        #         #remove loc if its an invalid result from "Term" column
-        #         if loc and cmd == 'request':
-        #             loc = None if loc.col == 1 else loc
-        #
-        #     if loc:
-        #         definition = ws.cell(loc.row, 2)
-        #
-        #         if definition.value:
-        #             send("@{}, the definition of {} is: \n\n{}".format(data['name'], loc.value, definition.value)
-        #                  , bot_info[0])
-        #         else:
-        #             send("@{}, term '{}' has been requested but not yet defined".
-        #                  format(data['name'], term), bot_info[0])
-        #
-        #     elif cmd == 'request':
-        #         try:
-        #             ws.append_row([term, '', '', "{}, {}".format(data['user_id'], data['name'])])
-        #         except:
-        #             send("@{}, an error occured when attempting to insert term '{}' into our database. Please contact"
-        #                  " a moderator".format(data['name'], term), bot_info[0])
-        #         else:
-        #             send("@{}, Term '{}' successfully added to our database, pending definition! "
-        #                  "Thank you for your contribution".format(data['name'], term), bot_info[0])
-        #     else:
-        #         send("@{} Term '{}' has not been requested or aliased yet. "
-        #              "You can use the command !request (phrase) to add it to our requests database!".
-        #              format(data['name'], term), bot_info[0])
